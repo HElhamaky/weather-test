210a3fbd007f26601324b519daf56e9aa090830c
hassan elhamaky <hassan.elhamaky@yahoo.com>
2020-09-11 11:18:30 +0200

    initial test4

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@
 const CACHE_NAME = "static-cache-v1";
 
 // CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = ["/offline.html, /css/style.css"];
+const FILES_TO_CACHE = ["/offline.html"];
 
 self.addEventListener("install", (evt) => {
   console.log("[ServiceWorker] Install");
